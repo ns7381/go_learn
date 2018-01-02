36ba3ceb425e5a82010eb01775b8c5842d846b79
ningsheng <ningsheng@inspur.com>
2018-01-03 01:31:18 +0800

    learn web

---
+++ src/app/backend/web.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:8080", nil)
+
+}
+
+func handler(rw http.ResponseWriter, req *http.Request)  {
+	req.ParseForm()
+	if len(req.Form["name"])> 0 {
+		fmt.Fprint(rw, "hello", req.Form["name"][0])
+	} else {
+		fmt.Fprint(rw, "hello world")
+	}
+
+}
+

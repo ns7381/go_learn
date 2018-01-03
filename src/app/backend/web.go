c29a6ddff964e55401ab4e585d06b4c2728ce5ff
ningsheng <ningsheng@inspur.com>
2018-01-03 23:40:35 +0800

    form submit

--- src/app/backend/web.go
+++ src/app/backend/web.go
@@ -3,18 +3,45 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"os"
+	"io"
 )
 
 func main()  {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/file", file)
 	http.ListenAndServe("localhost:8080", nil)
 
 }
 
-func handler(rw http.ResponseWriter, req *http.Request)  {
+func index(rw http.ResponseWriter, req *http.Request)  {
+	t, _ := template.ParseFiles("/Users/nathan/go/go_path/src/go_learn/src/app/backend/index.html")
+	t.Execute(rw, nil)
+}
+
+func login(rw http.ResponseWriter, req *http.Request)  {
 	req.ParseForm()
 	if len(req.Form["name"])> 0 {
 		fmt.Fprint(rw, "hello", req.Form["name"][0])
+	} else {
+		fmt.Fprint(rw, "hello world")
+	}
+
+}
+func file(rw http.ResponseWriter, req *http.Request)  {
+	if req.Method == "POST" {
+		req.ParseMultipartForm(32 << 20)
+		file, header, e := req.FormFile("file")
+		if e != nil {
+			fmt.Println(e)
+			return
+		}
+		defer file.Close()
+		openFile, _ := os.OpenFile("./"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		defer openFile.Close()
+		io.Copy(openFile, file)
 	} else {
 		fmt.Fprint(rw, "hello world")
 	}
